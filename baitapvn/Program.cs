baf4ad1336b70daaf6a60689f9a5aa48e1feca96
Admin <Admin@LAPTOP-EI74A7KG>
2024-01-19 09:15:42 +0700

    Add project files.

---
+++ baitapvn/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace baitapvn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SanPham sanPham = new SanPham();
+            Int In();
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baitapvn/SanPham.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapvn
+{
+    public class SanPham
+    {
+        public string MaSanPham {  get; set; }
+        public string TenSanPham {  get; set; }
+        public int  DonGia {  get; set; }  
+        public int  SoLuong {  get; set; }
+
+        public SanPham()
+        {
+
+        }
+
+        public SanPham(string maSanPham, string tenSanPham, int donGia, int soLuong)
+        {
+            MaSanPham = maSanPham;
+            TenSanPham = tenSanPham;
+            DonGia = donGia;
+            SoLuong = soLuong;
+        }
+
+        public void ThanhTien()
+        {
+            var thanhtien = this.DonGia * this.SoLuong;
+            Console.WriteLine("Thanh tien: " + thanhtien);
+        }
+
+        public void In()
+        {
+            Console.WriteLine("Ma san pham: " + this.MaSanPham);
+            Console.WriteLine("Ten san pham: " + this.TenSanPham);
+            Console.WriteLine("Don gia : " + this.DonGia);
+            Console.WriteLine("So luowng: " + this.SoLuong);
+        }
+    }
+}
